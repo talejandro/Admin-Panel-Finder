05a6b6bdec87d58fb2a7d7d29a358ae1b2ce1b2e
Tomas Alejandro <39777824+talejandro@users.noreply.github.com>
2022-01-25 01:59:25 -0300

    V0.0

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func linkfile(site string) {
+
+	LFile, err := os.Open("link.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fileScan := bufio.NewScanner(LFile)
+
+	for fileScan.Scan() {
+		page := site + "/" + fileScan.Text()
+
+		u, err := http.Get(page)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		if u.StatusCode != 404 {
+			fmt.Println(page)
+		}
+	}
+
+	LFile.Close()
+
+}
+
+func main() {
+
+	site := "https://itu.uncuyo.edu.ar"
+	linkfile(site)
+
+}
