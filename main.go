2640fd77c9a4d4c46f692ae57aba0ddc9133a0b7
Tomas Alejandro <39777824+talejandro@users.noreply.github.com>
2022-01-27 22:48:23 -0300

    Update main.go

--- main.go
+++ main.go
@@ -1,13 +1,3 @@
-package main
-
-import (
-	"bufio"
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-)
-
 func linkfile(site string) {
 
 	LFile, err := os.Open("link.txt")
@@ -16,27 +6,33 @@ func linkfile(site string) {
 		log.Fatal(err)
 	}
 	fileScan := bufio.NewScanner(LFile)
-
+	cont := 1
 	for fileScan.Scan() {
 		page := site + "/" + fileScan.Text()
-
+		cont = cont + 1
 		u, err := http.Get(page)
 
 		if err != nil {
 			log.Fatal(err)
 		}
-		if u.StatusCode != 404 {
-			fmt.Println(page)
+		if u.StatusCode == 200 {
+			fmt.Println(page, "-OK---", cont)
+		} else if u.StatusCode == 403 {
+			fmt.Println(page, "-Forbidden---", cont)
+		} else if u.StatusCode == 301 {
+			fmt.Println(page, "-Moved Permanently---", cont)
 		}
 	}
 
 	LFile.Close()
-
 }
-
 func main() {
+	inicio := time.Now()
 
-	site := "https:example.com"
+	site := "https://ejemplo.com"
+
 	linkfile(site)
 
-}
+	timetotal := time.Since(inicio)
+
+	fmt.Println(timetotal)
